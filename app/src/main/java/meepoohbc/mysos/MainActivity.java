0e2dd8340077ef04efeae1bbf2a85b953e94ab0c
Admin <poolovejane125@gmail.com>
2017-06-18 10:39:17 +0700

    ฉันได้ทำการ Itent ไปที่ หน้าสมัครสมาชิกเรียบร้อยแล้ว

--- app/src/main/java/meepoohbc/mysos/MainActivity.java
+++ app/src/main/java/meepoohbc/mysos/MainActivity.java
@@ -1,13 +1,51 @@
 package meepoohbc.mysos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //Explicit การประกาศตัวแปร
+    private EditText userEditText,passEditText;
+    private TextView textView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Initital View
+        initialView();
+
+        //TextView Controller จะทำให้ Textview กด คลิ๊กได้
+        textViewController();
+
+
+    }//Method Main กกกำห
+
+    private void textViewController() {
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //intent to RegisterActivity โค้ดไปลิ้งไปหน้า สทัครสมาชิก
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
-}
+
+    private void initialView() {
+
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passEditText = (EditText) findViewById(R.id.edtPass);
+        textView = (TextView) findViewById(R.id.txtRegister);
+        button = (Button) findViewById(R.id.btnLogin);
+    }
+}//Main Class นี่คือ คลาสหลัก
